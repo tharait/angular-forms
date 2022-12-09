75e18ce9a63c1290656473c71ea86d4434fa0af1
Sachintha <sachinthaslakshan@gmail.com>
2022-12-09 17:38:06 +0530

    first commit

---
+++ src/app/angular-forms/angular-forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-angular-forms',
+  templateUrl: './angular-forms.component.html',
+  styleUrls: ['./angular-forms.component.css'],
+})
+export class AngularFormsComponent {
+  models = ['Lambogini','Ferrari','BMW'];
+  colors = ['Red','Black','Blue'];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFormsComponent } from './angular-forms/angular-forms.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AngularFormsComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, CommonModule],
 })
-export class AppModule { }
+export class AppModule {}
